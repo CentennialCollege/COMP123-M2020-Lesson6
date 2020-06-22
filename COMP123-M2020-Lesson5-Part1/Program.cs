6ef6428866e34b8fc16209a0877f9189468a32b2
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-06-22 11:58:56 -0400

    updated class structures for project

--- COMP123-M2020-Lesson5-Part1/Program.cs
+++ COMP123-M2020-Lesson5-Part1/Program.cs
@@ -10,6 +10,14 @@ namespace COMP123_M2020_Lesson5_Part1
     {
         static void Main(string[] args)
         {
+            GameObject player = new GameObject("Player");
+
+            //player.transform.position = new Vector2D();
+
+            Console.WriteLine(player.ToString());
+
+            // listen for any key
+            Console.ReadLine();
         }
     }
 }
